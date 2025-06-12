bade3472d65ce58cd7a973f5b57d5eda256998b7
Pufminids <49758975+tdnphantom@users.noreply.github.com>
2025-06-12 15:12:57 +0700

    daily motd, kinda

---
+++ MOTD/motd-script/thursday.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+MOTD="|-----------------------------------------------------------------|
+| This system is for the use of AdMedika authorized users only.   |
+| Individuals using this computer system without authority, or in |
+| excess of their authority, are subject to having all of their   |
+| activities on this system monitored and recorded by system      |
+| personnel.                                                      |
+|                                                                 |
+| In the course of monitoring individuals improperly using this   |
+| system, or in the course of system maintenance, the activities  |
+| of authorized users may also be monitored.                      |
+|                                                                 |
+| Anyone using this system expressly consents to such monitoring  |
+| and is advised that if such monitoring reveals possible         |
+| evidence of criminal activity, system personnel may provide the |
+| evidence of such monitoring to law enforcement officials.       |
+|-----------------------------------------------------------------|
+Message of the Day:
+
+Kamis Optimis!
+Menepis sumber Krisis!
+ "
+
+echo "$MOTD" > /etc/motd
+echo "$MOTD" > /etc/issue
+echo "$MOTD" > /etc/issue.net
